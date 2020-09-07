df90d8b8b804809bc2659925b8ce8b447e173976
chika-dueke-eze <64172430+chika-dueke-eze@users.noreply.github.com>
2020-09-07 02:11:51 +0100

    Update run_beam3.py

--- run_beam3.py
+++ run_beam3.py
@@ -1,15 +1,3 @@
-"""
-[run_beam3.py]
-[Chika Dueke-Eze]
-[06-09-2020]
-Based on: RunCan.py
-Written by: Michael R. Gustafson II
-
-I understand and have adhered to all the tenets of the Duke Community Standard
-in creating this code.
-Signed: [cdg19]
-"""
-
 # %% import modules
 import numpy as np
 import pandas as pd
@@ -65,22 +53,7 @@ ax.set(
 # Use tight layout
 fig.tight_layout()
 
-# Save the graph as EPS and PDF
-fig.savefig('Beam3plot.eps')
-fig.savefig('Beam3plot.pdf')
-
-
-#%%% get the values we need in the proper notation
-for a in range(beam_data.size // 2):
-    print(
-        "{:0.4e} & {:0.4e} \\\\".format(
-         beam_data.iloc[a, 0], beam_data.iloc[a, 1]
-        )
-    )
-
-#values of p on line 34
-compliance = 0.00241634
-init_disp = 0.00058703
-
-slope_int = "{:0.4e} and {:0.4e}".format(compliance, init_disp)         #get the values in 5 significant figures
-print("The slope and intercept in scientific notation are", slope_int, "respectively")
+# Save the graph as EPS and PDF and PNG
+fig.savefig('Beam2plot.eps')
+fig.savefig('Beam2plot.pdf')
+fig.savefig('Beam2plot.png')
